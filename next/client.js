3ce3f79d54b63c6a1ca7dfa824f1553522edb48b
Ivan Fedulov <fedulov.ivan@gmail.com>
2019-12-02 23:39:02 +0300

    window size selector

--- next/client.js
+++ next/client.js
@@ -2,21 +2,37 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import nano from 'nano';
 import {
-    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
 } from 'recharts';
 import moment from 'moment';
+import * as d3 from 'd3';
 
 const dbclient = nano('http://localhost:5984');
 const db = dbclient.use('mqtt');
 
+const MINUTE = 60 * 1000;
+const HOUR = 3600 * 1000;
+const DAY = HOUR * 24;
+
 function Root() {
 
+    const historyOptions = [
+        { name: "1 minute", value: MINUTE },
+        { name: "30 minutes", value: MINUTE * 30 },
+        { name: "1 hour", value: HOUR },
+        { name: "4 hours", value: HOUR * 4 },
+        { name: "1 day", value: DAY },
+    ];
+
     const [docs, setDocs] = useState([])
+
+    const [historyOption, setHistoryOption] = useState(HOUR);
 
     useEffect(() => {
         const query = {
             selector: {
-                co2: { "$gt": 0 }
+                // co2: { "$gt": 0 }
+                timestamp: { "$gt": (new Date()).valueOf() - historyOption }
             },
             fields: ["co2", "timestamp"],
             limit: 10000
@@ -24,23 +40,60 @@ function Root() {
         db.find(query).then(response => {
             setDocs(response.docs);
         });
-    }, []);
+    }, [historyOption]);
 
-    console.log(docs);
+
+    // const now = new Date();
+    // const domainToday = d3.scaleTime().domain([d3.timeDay.floor(now), d3.timeDay.ceil(now)]);
+    // const timeFormatter = (tick) => {return d3.timeFormat('%H:%M:%S')(new Date(tick));};
+    // const ticks = domainToday.ticks(d3.timeHour.every(1));
 
     return (
-        <LineChart width={1200} height={300} data={docs}>
-            <Tooltip />
-            <Legend />
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis name="Time" dataKey="timestamp" interval={100} tickFormatter={v => {
-                return moment(v).format('HH:mm');
-                // const d = new Date(v);
-                // return `${d.getHours()}:${d.getMinutes()}`;
-            }} />
-            <YAxis name="CO2" />
-            <Line dataKey="co2" dot={false}/>
-        </LineChart>
+        <div>
+            <ResponsiveContainer width = "100%" height = {500}>
+                <LineChart data={docs}>
+                    <Tooltip />
+                    <Legend />
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis
+
+                        // domain={domainToday}
+                        // ticks={ticks}
+                        // tickFormatter={timeFormatter}
+
+                        dataKey="timestamp"
+
+                        // domain = {['auto', 'auto']}
+                        name = 'Time'
+                        tickFormatter = {(unixTime) => moment(unixTime).format('HH:mm:ss')}
+                        // type = 'number'
+
+                        // scale='time'
+                        // type='number'
+                        // interval={300}
+                        // name="Time"
+                        // tickFormatter={v => {
+                        //     return moment(v).format('HH:mm:ss');
+                        // }}
+
+                    />
+                    <YAxis name="CO2" />
+                    <Line dataKey="co2" dot={false}/>
+                </LineChart>
+            </ResponsiveContainer>
+            <select
+                onChange={e => setHistoryOption(Number(e.target.value))}
+                value={historyOption}
+            >
+                {historyOptions.map(item => {
+                    return (
+                        <option key={item.name} value={item.value}>
+                            {item.name}
+                        </option>
+                    );
+                })}
+            </select>
+        </div>
     );
 
 }
